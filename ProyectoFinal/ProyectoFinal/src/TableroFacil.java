3b984d52972c479b1386528b104eaea4b44ae184
kmarcela11 <114821688+kmarcela11@users.noreply.github.com>
2022-11-13 12:15:03 -0500

    le puse un boton para poder pasar al frame de tablero facil, pero no se porque no funciona aaaaaaaaaaaaaaaaaa jajajaja , capaz y es mi netbeans

--- ProyectoFinal/ProyectoFinal/src/TableroFacil.java
+++ ProyectoFinal/ProyectoFinal/src/TableroFacil.java
@@ -1,4 +1,3 @@
-
 import java.awt.Color;
 import java.awt.Graphics;
 import static java.awt.MouseInfo.getPointerInfo;
@@ -8,11 +7,10 @@ import java.util.Scanner;
 import javax.swing.JOptionPane;
 
 public class TableroFacil extends javax.swing.JFrame {
-
     public TableroFacil() {
         initComponents();
+        
     }
-
     Scanner lea = new Scanner(System.in);
     int xpos = 0;
     int ypos = 0;
@@ -414,7 +412,6 @@ public class TableroFacil extends javax.swing.JFrame {
             }
             System.out.print(" \n");
         }
-
         System.out.print(" \n");
     }//GEN-LAST:event_mostrarmtActionPerformed
 
@@ -526,37 +523,8 @@ public class TableroFacil extends javax.swing.JFrame {
 
     }
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(TableroFacil.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(TableroFacil.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(TableroFacil.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(TableroFacil.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-        //</editor-fold>
-        //</editor-fold>
-        //</editor-fold>
 
-        /* Create and display the form */
+    public static void main(String args[]) {
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
                 new TableroFacil().setVisible(true);
