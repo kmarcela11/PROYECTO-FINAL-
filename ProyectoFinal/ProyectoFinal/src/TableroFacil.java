1b81e9672f2f9fd2a1b61b58c4d4cb3fdb53dda1
valentina <vecabrera@uninorte.edu.co>
2022-11-14 10:55:12 -0500

    tiene musica

--- ProyectoFinal/ProyectoFinal/src/TableroFacil.java
+++ ProyectoFinal/ProyectoFinal/src/TableroFacil.java
@@ -4,16 +4,39 @@ import java.awt.Graphics;
 import static java.awt.MouseInfo.getPointerInfo;
 import java.awt.Point;
 import java.awt.event.KeyEvent;
+import java.io.IOException;
 import java.util.Random;
 import java.util.Scanner;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 
+
 public class TableroFacil extends javax.swing.JFrame {
 
+    
+    public Clip clip;
+    public String link = "/sonido/";
+    
     public TableroFacil() {
         initComponents();
         this.setLocationRelativeTo(null);
+    }
+    
+    public void mus(String archivo){
+        
+        try{
+            
+            clip = AudioSystem.getClip();
+            clip.open(AudioSystem.getAudioInputStream(getClass().getResourceAsStream(link+ archivo+ ".wav")));
+            clip.start();
+        }
+        catch(IOException | LineUnavailableException | UnsupportedAudioFileException e){
+            
+        }
     }
     
     Scanner lea = new Scanner(System.in);
@@ -56,6 +79,7 @@ public class TableroFacil extends javax.swing.JFrame {
         Tablero = new javax.swing.JPanel();
         jLabel4 = new javax.swing.JLabel();
         jButton3 = new javax.swing.JButton();
+        jButton4 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         addKeyListener(new java.awt.event.KeyAdapter() {
@@ -187,6 +211,13 @@ public class TableroFacil extends javax.swing.JFrame {
             }
         });
 
+        jButton4.setText("musica");
+        jButton4.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton4ActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
@@ -211,9 +242,11 @@ public class TableroFacil extends javax.swing.JFrame {
                                 .addGroup(javax.swing.GroupLayout.Alignment.LEADING, jPanel1Layout.createSequentialGroup()
                                     .addGap(20, 20, 20)
                                     .addComponent(jLabel4))))
-                        .addGap(56, 56, 56)
-                        .addComponent(jButton1)))
-                .addContainerGap(220, Short.MAX_VALUE))
+                        .addGap(55, 55, 55)
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addComponent(jButton1)
+                            .addComponent(jButton4))))
+                .addContainerGap(241, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -235,7 +268,9 @@ public class TableroFacil extends javax.swing.JFrame {
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                         .addComponent(Tablero, javax.swing.GroupLayout.PREFERRED_SIZE, 500, javax.swing.GroupLayout.PREFERRED_SIZE))
                     .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGap(343, 343, 343)
+                        .addGap(270, 270, 270)
+                        .addComponent(jButton4)
+                        .addGap(51, 51, 51)
                         .addComponent(jButton1)))
                 .addContainerGap(79, Short.MAX_VALUE))
         );
@@ -503,6 +538,12 @@ public class TableroFacil extends javax.swing.JFrame {
         
     }//GEN-LAST:event_jButton3ActionPerformed
 
+    private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
+        // TODO add your handling code here:
+       mus("musica");
+        
+    }//GEN-LAST:event_jButton4ActionPerformed
+
     private int PosMatriz(int poscursor, int filcol, int medida) {
         // Función que retorna la posición del cursor en la matriz.
         int k = 0;
@@ -653,6 +694,7 @@ public class TableroFacil extends javax.swing.JFrame {
     private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton2;
     private javax.swing.JButton jButton3;
+    private javax.swing.JButton jButton4;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel4;
