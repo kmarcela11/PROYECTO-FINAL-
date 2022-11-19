b16f5b4468dec46144cc3f2e018653016aa2d5b9
ValenGaymer <valenbustamanteq@gmail.com>
2022-11-19 17:09:35 -0500

    ADELANTOO

--- ProyectoFinal/ProyectoFinal/src/HOMEE.java
+++ ProyectoFinal/ProyectoFinal/src/HOMEE.java
@@ -1,50 +1,77 @@
+
+import javax.swing.ImageIcon;
+
 public class HOMEE extends javax.swing.JFrame {
+
     public HOMEE() {
         initComponents();
         this.setLocationRelativeTo(null);
     }
 
-
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jButton1 = new javax.swing.JButton();
+        gametitle = new javax.swing.JLabel();
+        playername = new javax.swing.JLabel();
+        selectlevel_bt = new javax.swing.JLabel();
+        bghome = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+        getContentPane().add(gametitle, new org.netbeans.lib.awtextra.AbsoluteConstraints(460, 100, 700, 130));
 
-        jButton1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/pfmedia/tempb.png"))); // NOI18N
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
+        playername.setIcon(new javax.swing.ImageIcon(getClass().getResource("/pfmedia/x.png"))); // NOI18N
+        playername.setToolTipText("");
+        playername.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        playername.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                playernameMouseClicked(evt);
             }
         });
+        getContentPane().add(playername, new org.netbeans.lib.awtextra.AbsoluteConstraints(770, 250, 340, 80));
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGap(91, 91, 91)
-                .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 341, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(158, Short.MAX_VALUE))
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(155, Short.MAX_VALUE)
-                .addComponent(jButton1)
-                .addGap(38, 38, 38))
-        );
+        selectlevel_bt.setIcon(new javax.swing.ImageIcon(getClass().getResource("/pfmedia/selectlevel_bt.png"))); // NOI18N
+        selectlevel_bt.setToolTipText("");
+        selectlevel_bt.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        selectlevel_bt.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                selectlevel_btMouseClicked(evt);
+            }
+            public void mouseEntered(java.awt.event.MouseEvent evt) {
+                selectlevel_btMouseEntered(evt);
+            }
+            public void mouseExited(java.awt.event.MouseEvent evt) {
+                selectlevel_btMouseExited(evt);
+            }
+        });
+        getContentPane().add(selectlevel_bt, new org.netbeans.lib.awtextra.AbsoluteConstraints(770, 310, -1, 80));
+
+        bghome.setIcon(new javax.swing.ImageIcon(getClass().getResource("/pfmedia/homebg.png"))); // NOI18N
+        getContentPane().add(bghome, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 1200, 720));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+    private void selectlevel_btMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_selectlevel_btMouseClicked
         USUARIO abrir = new USUARIO();
         this.setVisible(false);
         abrir.setVisible(true);
-    }//GEN-LAST:event_jButton1ActionPerformed
+    }//GEN-LAST:event_selectlevel_btMouseClicked
+
+    private void playernameMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_playernameMouseClicked
+
+    }//GEN-LAST:event_playernameMouseClicked
+
+    private void selectlevel_btMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_selectlevel_btMouseEntered
+        ImageIcon im = new ImageIcon(getClass().getResource("/pfmedia/selectlevel_btpressed.png"));
+        selectlevel_bt.setIcon(im);
+    }//GEN-LAST:event_selectlevel_btMouseEntered
+
+    private void selectlevel_btMouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_selectlevel_btMouseExited
+        ImageIcon im = new ImageIcon(getClass().getResource("/pfmedia/selectlevel_bt.png"));
+        selectlevel_bt.setIcon(im);
+    }//GEN-LAST:event_selectlevel_btMouseExited
 
     public static void main(String args[]) {
 
@@ -56,6 +83,9 @@ public class HOMEE extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
+    private javax.swing.JLabel bghome;
+    private javax.swing.JLabel gametitle;
+    private javax.swing.JLabel playername;
+    private javax.swing.JLabel selectlevel_bt;
     // End of variables declaration//GEN-END:variables
 }
